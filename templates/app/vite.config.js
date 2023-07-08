afdde290d66bb29ac46142ce651812ee88dfde5b
stone <scouserlfc91@gmail.com>
2023-07-08 18:56:38 +0200

    еще файлы

--- templates/app/vite.config.js
+++ templates/app/vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from 'tailwindcss';
-import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(),splitVendorChunkPlugin()],
+	plugins: [vue()],
 	build: {
 		outDir: '../../public/assets',
 		rollupOptions: {
