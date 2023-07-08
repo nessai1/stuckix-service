c3049fc28a37e3960f03022c6a770853bddc44f1
Artyom <artyomrogov2012@gmail.com>
2023-07-08 13:58:45 +0200

    список тегов ошибки

---
+++ templates/app/src/components/issue/TagItem.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps(['tag']);
+</script>
+
+<template>
+<div class="tag-container mr-2">
+  <div class="tag-key">{{props.tag.key}}</div>
+  <div class="tag-delimiter"></div>
+  <div class="tag-value">{{props.tag.value}}</div>
+</div>
+</template>
+
+<style scoped>
+.tag-container {
+  display: inline-flex;
+  padding: 2px 8px;
+  align-items: center;
+  gap: 8px;
+  border-radius: 6px;
+  border: 1px solid #F1F3F5;
+  background: #FFF;
+}
+
+.tag-delimiter {
+  width: 1px;
+  align-self: stretch;
+  background: #DEE2E6;
+}
+
+.tag-value {
+  color: #269AFF;
+}
+</style>
---
+++ templates/app/src/components/issue/TagList.vue
@@ -0,0 +1,34 @@
+<script setup>
+import TagItem from "./TagItem.vue";
+
+// const props = defineProps(['tags']);
+const props = {};
+props.tags = [
+  {
+    key: 'среда',
+    value: 'обрабатывается',
+  },
+  {
+    key: 'обрабатывается',
+    value: 'да',
+  },
+  {
+    key: 'уровень',
+    value: 'ошибка',
+  },
+]
+
+</script>
+
+<template>
+  <div class="font-semibold text-slate-900 text-2xl mb-4">Теги</div>
+  <TagItem
+      v-for="tag in props.tags"
+      :key="tag.key"
+      :tag="tag"
+  />
+</template>
+
+<style scoped>
+
+</style>
