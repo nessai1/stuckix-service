3f57791f04462732468e6e25dcd1e3f01c2dc0bf
nessai1 <werowkin@gmail.com>
2023-07-08 10:13:03 +0200

    Билд нескольких проектов

--- templates/app/vite.config.js
+++ templates/app/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        login: resolve(__dirname, 'login.html'),
+        projects: resolve(__dirname, 'projects.html'),
+      },
+    },
+  }
+});
