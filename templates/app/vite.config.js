7f53f1c29cd899f396bab73d5b7a5b59320088f2
stone <scouserlfc91@gmail.com>
2023-07-08 17:46:46 +0200

    автобилд от vite не будет создавать файлы с хэшем

--- templates/app/vite.config.js
+++ templates/app/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      input: {
-        login: resolve(__dirname, 'login.html'),
-        projects: resolve(__dirname, 'projects.html'),
-      },
-    },
-  }
+	plugins: [vue()],
+	build: {
+		outDir: '../../public/assets',
+		rollupOptions: {
+			input: {
+				login: resolve(__dirname, 'login.html'),
+				projects: resolve(__dirname, 'projects.html')
+			},
+			output: {
+				entryFileNames: '[name].js', // Имя файла без хеша для каждого входного файла
+				chunkFileNames: '[name].js', // Имя файла без хеша для чанков
+				assetFileNames: '[name].[ext]' // Имя файла без хеша для статических ресурсов (например, изображений)
+			}
+		}
+	}
 });
