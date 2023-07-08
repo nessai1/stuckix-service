19c2ff2990b9a9d0c7ebcf71eff40185ae5a7a56
nessai1 <werowkin@gmail.com>
2023-07-08 10:42:32 +0200

    Передаем пропсы из бэка в компонент

---
+++ templates/app/src/Issue.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps(['some']);
+</script>
+
+<template>
+  <div>{{props.some}}</div>
+</template>
+
+<style scoped>
+</style>
---
+++ templates/app/src/issue.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.css'
+import Issue from "./Issue.vue";
+
+createApp(Issue, window.pageParams).mount('#app')
