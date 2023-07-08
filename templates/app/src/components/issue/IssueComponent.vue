9bddff11dc69dd12ddd28d81ae1bebfb89613e08
Artyom <artyomrogov2012@gmail.com>
2023-07-08 13:47:33 +0200

    блоки про окружение ошибки

--- templates/app/src/components/issue/IssueComponent.vue
+++ templates/app/src/components/issue/IssueComponent.vue
@@ -3,10 +3,13 @@
     <Header />
     <br>
     <br>
+    <EnvironmentList/>
     <CodeBlock/>
+
   </div>
 </template>
 <script setup>
 import Header from "./Header.vue";
 import CodeBlock from "./codeblock/CodeBlock.vue";
+import EnvironmentList from "./EnvironmentList.vue";
 </script>
