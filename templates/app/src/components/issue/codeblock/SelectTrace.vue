65d14f1bcac6f44b842d05ee985df251fc7840dc
nessai1 <werowkin@gmail.com>
2023-07-08 14:25:02 +0200

    stacktrace selector

---
+++ templates/app/src/components/issue/codeblock/SelectTrace.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+  props: {
+    row: String,
+    file: String,
+    selected: Boolean
+  }
+}
+</script>
+<template>
+  <li
+      :class="selected ? 'selected' : ''"
+  >
+    <div class="row">
+      {{ row }}
+    </div>
+    <div class="file">
+      <span>
+        {{ file }}
+      </span>
+    </div>
+  </li>
+</template>
+<style lang="scss" scoped>
+li {
+
+  &:hover {
+    background-color: #DEF6FF;
+  }
+  cursor: pointer;
+  background-color: #F0FBFF;
+  width: max-content;
+  display: flex;
+
+  .row {
+    color: #2FC6F6;
+    padding: 19px;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -0.024px;
+    border-right: 1px #A5E9FF solid;
+  }
+
+  .file {
+    padding: 8px;
+    color: #009BCC;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -0.028px;
+  }
+}
+
+.selected {
+  background-color: #DEF6FF !important;
+  .row {
+    border-right: 1px #2FC6F6 solid !important;
+  }
+}
+
+</style>
--- templates/app/src/components/issue/codeblock/Stack.vue
+++ templates/app/src/components/issue/codeblock/Stack.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
-    selector
-  </div>
-  <div>
-    code
+  <div class="stack">
+    <ul class="w-8/12 selector">
+      <SelectTrace row="12" file="E:\hrtoolkit\vendor\laravel\framework\src\illuminateadadasdasdasd" />
+      <SelectTrace :selected="true" row="12" file="E:\hrtoolkit\vendor\laravel\framework\src\illuminateadadasdasdasd" />
+      <SelectTrace row="12" file="E:\hrtoolkit\vendor\laravel\framework\src\illuminateadadasdasdasd" />
+    </ul>
+    <div class="w-full code">
+      code
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+  .stack {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .selector {
+    max-height: 370px;
+    overflow: scroll;
+  }
+</style>
+<script setup>
+import SelectTrace from "./SelectTrace.vue";
+</script>
