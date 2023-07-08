3f3b78d72a3565dd13904a04675e21eefbdedc50
Artyom <artyomrogov2012@gmail.com>
2023-07-08 12:31:40 +0200

    проброс проектов через pageParams

--- templates/app/src/Projects.vue
+++ templates/app/src/Projects.vue
@@ -3,18 +3,7 @@
 import ProjectsList from "./components/ProjectsList.vue";
 import HeadWrapper from "./components/header/HeadWrapper.vue";
 
-const projects = [
-  {
-    id: 1,
-    title: 'First project',
-    description: 'prkdsfgsdpjfdsjg opsdj vs  sdopsdjv ',
-  },
-  {
-    id: 2,
-    title: 'second project',
-    description: 'prkdsfgsdpjfdsjg opsdj vs  sdopsdjv ',
-  },
-]
+const props = defineProps(['projects']);
 </script>
 
 <template>
