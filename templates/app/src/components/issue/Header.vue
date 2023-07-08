1d716524b42cb45f964cddb0f4c2722c9ede4353
EJGPM2320100041\r.gainullin <r.gainullin@bitrix.ru>
2023-07-08 13:20:39 +0200

    хэдер страницы ошибок

--- templates/app/src/components/issue/Header.vue
+++ templates/app/src/components/issue/Header.vue
@@ -1,3 +1,29 @@
 <template>
-  Head data
+  <div class="header-container w-full h-min pt-4 pl-10">
+    <div class="flex text-gray-300">
+      <span>Ошибки</span>
+      <img src="../../../public/icons/arrow-up-s-line.svg" class="ml-3">
+      <img src="../../../public/icons/folder-5-fill.svg" class="ml-3">
+      <span class="ml-2">Навзвание проекта</span>
+    </div>
+
+    <div class="flex mt-4 items-end">
+      <span class="font-semibold text-2xl">Ошибка</span>
+      <span class="font-medium text-gray-500 ml-3 pb-0.5">/index.php в строке 25</span>
+    </div>
+
+    <div class="flex mt-4">
+      <div class="red-dot border bg-red-600 rounded-full mt-2"></div>
+      <span class="ml-3 w-1/2 text-gray-600 text-left">
+        Описание ошибки в строчку, длинное описание ошибки, описание ошибки в две строки
+      </span>
+    </div>
+  </div>
 </template>
+
+<style>
+.red-dot{
+  height: 10px;
+  width: 10px;
+}
+</style>
